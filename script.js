87cbed367113bee321085c510a263678d2666585
Maedeh Sadat Khorasani <maedeh.skh80@gmail.com>
2025-11-09 21:07:18 +0330

    finish all js functions

--- script.js
+++ script.js
@@ -12,6 +12,41 @@ function changes() {
     const numbersofCharacters = textareaEl.value.length;
     numbersofCharactersEl.textContent = numbersofCharacters;
   }
+
+  // letters alowed for twitter
+  function twitter() {
+    const numberofTwitter = 280 - textareaEl.value.length;
+    numberoftwitterEl.textContent = numberofTwitter;
+    if (numberofTwitter < 0) {
+      numberoftwitterEl.classList.add("state-number--limite");
+    } else {
+      numberoftwitterEl.classList.remove("state-number--limite");
+    }
+  }
+
+  // letters alowed for facebook
+  function facebook() {
+    const numberofFacebook = 2200 - textareaEl.value.length;
+    numberoffacebookEl.textContent = numberofFacebook;
+
+    if (numberofFacebook < 0) {
+      numberoffacebookEl.classList.add("state-number--limite");
+    } else {
+      numberoffacebookEl.classList.remove("state-number--limite");
+    }
+  }
+
+  function words() {
+    const numberofwords = textareaEl.value.split(" ").length;
+    numberofwordsEl.textContent = numberofwords;
+    if (textareaEl.value.length === 0) {
+      numberofwordsEl.textContent = 0;
+    }
+  }
+
   characters();
+  twitter();
+  facebook();
+  words();
 }
 textareaEl.addEventListener("input", changes);
